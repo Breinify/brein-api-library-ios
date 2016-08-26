0de318d61977f1ea6147bfdad0e5f01b74d80458
Marco <marco.recchioni@inform-ac.com>
2016-08-26 13:41:34 +0200

    sorry, need to delete it again.

--- Example/Pods/Target Support Files/BreinifyApi/BreinifyApi-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_BreinifyApi : NSObject
-@end
-@implementation PodsDummy_BreinifyApi
-@end
--- Example/Pods/Target Support Files/BreinifyApi/BreinifyApi-umbrella.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "BreinifyApi-Bridging-Header.h"
-
-FOUNDATION_EXPORT double BreinifyApiVersionNumber;
-FOUNDATION_EXPORT const unsigned char BreinifyApiVersionString[];
-
