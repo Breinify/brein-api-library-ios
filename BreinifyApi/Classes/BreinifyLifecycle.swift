7c731b0b2520ca72397d74ca0ee966e28c07aaf0
Marco <m.recchioni@me.com>
2017-04-06 20:47:54 +0200

    extension to support lifecycle with class Breinfiy

---
+++ BreinifyApi/Classes/BreinifyLifecycle.swift
@@ -0,0 +1,44 @@
+//
+// Created by Marco 
+//
+// Intended to support the iOS Application Lifecycle
+
+
+import Foundation
+
+public extension Breinify {
+
+    open class func didFinishLaunchingWithOptions(apiKey: String, secret: String) {
+
+        // delegate to the BreinifyManager
+        BreinifyManager.sharedInstance.didFinishLaunchingWithOptions(apiKey: apiKey, secret: secret)
+    }
+
+
+    open class func applicationDidEnterBackground() {
+        
+        BreinifyManager.sharedInstance.applicationDidEnterBackground()
+    }
+    
+    open class func applicationWillTerminate() {
+        
+        BreinifyManager.sharedInstance.applicationWillTerminate()
+    }
+    
+    open class func didRegisterForRemoteNotificationsWithDeviceToken(_ deviceToken: Data) ->  String! {
+        
+        let token = BreinifyManager.sharedInstance.didRegisterForRemoteNotificationsWithDeviceToken(deviceToken)
+        return token
+    }
+    
+    open class func didFailToRegisterForRemoteNotificationsWithError(_ error: Error) {
+        
+        BreinifyManager.sharedInstance.didFailToRegisterForRemoteNotificationsWithError(error)
+    }
+    
+    open class func didReceiveRemoteNotification(_ userInfo: [AnyHashable: Any], _ viewController: UIViewController?) {
+        
+        BreinifyManager.sharedInstance.didReceiveRemoteNotification(userInfo, viewController)
+    }
+    
+}
