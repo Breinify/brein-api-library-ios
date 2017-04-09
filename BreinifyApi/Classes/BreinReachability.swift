e4f88d5a0acaa44eeb9744103e362ae94053ac90
Marco <m.recchioni@me.com>
2017-04-09 13:34:18 +0200

    support of network detection

---
+++ BreinifyApi/Classes/BreinReachability.swift
@@ -0,0 +1,39 @@
+//
+//  BreinReachability.swift
+//  Pods
+//
+//  Created by Marco on 07.04.17.
+//
+//
+
+import Foundation
+import SystemConfiguration
+
+open class BreinReachability {
+    
+    class func connectedToNetwork() -> Bool {
+        
+        var zeroAddress = sockaddr_in()
+        zeroAddress.sin_len = UInt8(MemoryLayout<sockaddr_in>.size)
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        
+        guard let defaultRouteReachability = withUnsafePointer(to: &zeroAddress, {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {
+                SCNetworkReachabilityCreateWithAddress(nil, $0)
+            }
+        }) else {
+            return false
+        }
+        
+        var flags: SCNetworkReachabilityFlags = []
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability, &flags) {
+            return false
+        }
+        
+        let isReachable = flags.contains(.reachable)
+        let needsConnection = flags.contains(.connectionRequired)
+        
+        return (isReachable && !needsConnection)
+    }
+    
+}
