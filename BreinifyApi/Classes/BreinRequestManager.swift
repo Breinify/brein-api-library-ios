79a15497d24b2ac94e35ce85068f0a43273afe58
Marco <m.recchioni@me.com>
2017-04-11 23:40:04 +0200

    correct handling of saved and loaded requests

--- BreinifyApi/Classes/BreinRequestManager.swift
+++ BreinifyApi/Classes/BreinRequestManager.swift
@@ -25,19 +25,30 @@ open class BreinRequestManager {
         updateTimer?.invalidate()
         updateTimer = nil
     }
-    
-    // dictionary of unixTimestamp + jsonRequest (unixTimestamp, fullUrl, jsonString)
-    var missedRequests = [JsonRequest]()
+
+    // dictionary of UUID to jsonRequest (unixTimestamp, fullUrl, jsonString)
+    var missedRequests = [String: JsonRequest]()
 
     // contains the unixTimestamp when the failure was detected
     var failureTime = 0.0
 
-    // contains the expiration Duration (5 minutes)
-    var expirationDuration = 5 * 60
+    // contains the expiration Duration (5 minutes in seconds)
+    var expirationDuration = 5 * 60 * 60
 
+    /// instance for processing unsent requests
     var updateTimer: Timer?
 
-    var interval = 10.0
+    /// contains the background interval
+    var interval = 60.0
+
+    /// contains the delimiter between each element of the structure
+    let kDelimiter = "°"
+
+    /// contains the filename for saved requests
+    let kRequestFileName = "BreinifyMissedRequests"
+
+    /// contains the full filename for save requests
+    let kRequestFullFileName = "BreinifyMissedRequests.txt"
 
     public func configure() {
         updateTimer = Timer.scheduledTimer(timeInterval: interval,
@@ -46,33 +57,37 @@ open class BreinRequestManager {
                 userInfo: nil,
                 repeats: true)
     }
-    
+
     @objc
     public func sendActivityRequests() {
         print("sendActivityRequests invoked")
         print("number of missed requests are: \(self.missedRequests.count)")
 
-        if  missedRequests.count > 0 {
+        if missedRequests.count > 0 {
             print("invoking new request...")
             Breinify.getConfig().getBreinEngine().getRestEngine().executeSavedRequests()
         }
     }
 
     public func addRequest(timeStamp: Int, fullUrl: String?, json: String?) {
-        let jsonRequest = JsonRequest()
+        var jsonRequest = JsonRequest()
+        // UUID
+        let uuid = UUID().uuidString
         jsonRequest.creationTime = timeStamp
         jsonRequest.fullUrl = fullUrl
         jsonRequest.jsonBody = json
 
-        missedRequests.append(jsonRequest)
+        missedRequests[uuid] = jsonRequest
+
+        print("Adding to Queue: \(uuid)")
     }
 
-    public func getMissedRequestArray() -> [JsonRequest] {
+    public func getMissedRequests() -> [String: JsonRequest] {
         return self.missedRequests
     }
 
     public func clearMissedRequests() {
-        self.missedRequests = [JsonRequest]()
+        self.missedRequests = [String: JsonRequest]()
     }
 
     public func status() -> String {
@@ -80,47 +95,127 @@ open class BreinRequestManager {
         return numberOfRequests
     }
 
+    /**
+
+        Removes an entry from the missed requests dictionary
+
+    */
+    public func removeEntry(_ key: String) {
+
+        print("size before is: \(missedRequests.count)")
+        missedRequests.removeValue(forKey: key)
+        print("size after is: \(missedRequests.count)")
+
+    }
+
+    /**
+    
+        Checks if currentEntry is in time range
+ 
+    */
+    public func checkIfValid(currentEntry: JsonRequest) -> Bool {
+
+        let creationTime = currentEntry.creationTime
+        let nowTime = Int(NSDate().timeIntervalSince1970)
+        let pastTime = nowTime - expirationDuration
+
+        if creationTime! > pastTime {
+            return true
+        }
+
+        return false
+    }
+
+    /**
+
+        Save the missed requested if necessary
+
+    */
     public func shutdown() {
         safeMissedRequests()
     }
 
+    /**
+
+        This method saves the missed requests to file with the following structure:
+
+        value-creationtime; value-fullUrl; value-jsonBody
+
+        The key can be ignored because when the missed requests will be loaded a new
+        key will be generated.
+
+    */
     public func safeMissedRequests() {
+
         if self.missedRequests.count > 0 {
 
             let fileURL = try! FileManager.default.url(for: .documentDirectory,
                             in: .userDomainMask, appropriateFor: nil, create: false)
-                    .appendingPathComponent("BreinifyMissedRequests.txt")
+                    .appendingPathComponent(kRequestFullFileName)
 
             if let outputStream = OutputStream(url: fileURL, append: true) {
                 outputStream.open()
 
                 var output = ""
-                for jsonElement in self.missedRequests {
+
+                for (key, jsonElement) in self.missedRequests {
                     output = output
-                            + "\(jsonElement.creationTime)"
-                            + ";"
+                            + "\(jsonElement.creationTime!)"
+                            + kDelimiter
                             + jsonElement.fullUrl
-                            + ";"
+                            + kDelimiter
                             + jsonElement.jsonBody
                             + "\n"
+
+                    let bytesWritten = outputStream.write(output,
+                            maxLength: output.lengthOfBytes(using: .utf8))
+                    if bytesWritten < 0 {
+                        print("write failure")
+                    }
                 }
-                let bytesWritten = outputStream.write(output,
-                        maxLength: output.lengthOfBytes(using: .utf8))
-                if bytesWritten < 0 {
-                    print("write failure")
-                }
+
                 outputStream.close()
             } else {
                 print("Unable to open file")
             }
         }
+
+        // delete file if no entries exists
+        if self.missedRequests.count == 0 {
+
+            let fileManager = FileManager.default
+            let fileName = kRequestFileName
+            let docDirectory = try? fileManager.url(for: .documentDirectory, in: .userDomainMask,
+                    appropriateFor: nil, create: true)
+
+            if let fileURL = docDirectory?.appendingPathComponent(fileName).appendingPathExtension("txt") {
+
+                let filePathName = fileURL.path
+
+                do {
+                    try fileManager.removeItem(atPath: filePathName)
+                } catch {
+                    print("Could not remove file: \(filePathName)")
+                }
+                    
+            }
+        }
     }
 
+    /**
+    
+        This method loads the saved requests from file. Assuming the following structure:
+
+        value-creationtime; value-fullUrl; value-jsonBody
+
+        For each loaded line a new key will be generated.
+
+    */
     public func loadMissedRequests() {
 
         if self.missedRequests.count == 0 {
 
-            let fileName = "BreinifyMissedRequests"
+            let fileName = kRequestFileName
             let docDirectory = try? FileManager.default.url(for: .documentDirectory, in: .userDomainMask,
                     appropriateFor: nil, create: true)
 
@@ -135,15 +230,15 @@ open class BreinRequestManager {
                     let fileContent = inString.components(separatedBy: .newlines)
 
                     for line in fileContent {
-                        let element = line.components(separatedBy: ";")
+                        let elements = line.components(separatedBy: kDelimiter)
 
-                        if element.count >= 3 {
-                            print(element)
-                            let unixTimestamp = Int(element[0])!
-                            let fullUrl = element[1]
-                            let jsonBody = element[2]
+                        if elements.count >= 3 {
+                            let unixTimestampAsInt: String = elements[0]
+                            let unixTimestamp = Int(unixTimestampAsInt)
+                            let fullUrl = elements[1]
+                            let jsonBody = elements[2]
 
-                            addRequest(timeStamp: unixTimestamp, fullUrl: fullUrl, json: jsonBody)
+                            addRequest(timeStamp: unixTimestamp!, fullUrl: fullUrl, json: jsonBody)
                         }
                     }
                 } catch {
