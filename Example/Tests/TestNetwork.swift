4d45563844e7ceff2496e4133228e698c2857b5c
Marco <m.recchioni@me.com>
2017-04-12 16:16:41 +0200

    additional testcase

--- Example/Tests/TestNetwork.swift
+++ Example/Tests/TestNetwork.swift
@@ -35,8 +35,68 @@ class TestNetwork: XCTestCase {
     }
 
 
-    func detectFlightMode() {
+    func testIp() {
+
+        let ip = detectIp()
+        print("IP is: \(ip)")
         
+    }
+    
+    func detectIp() -> Bool {
+
+        var zeroAddress = sockaddr_in()
+        zeroAddress.sin_len = UInt8(MemoryLayout<sockaddr_in>.size)
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+
+        guard let defaultRouteReachability = withUnsafePointer(to: &zeroAddress, {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {
+                SCNetworkReachabilityCreateWithAddress(nil, $0)
+            }
+        }) else {
+            return false
+        }
+
+        print(defaultRouteReachability)
+        print(zeroAddress)
+
+        return false
+    }
+
+    
+    func testFlightMode() {
+
+        let networkConnected = connectedToNetwork()
+        if networkConnected == true {
+            print("Internet connection available")
+        } else {
+            print("No internet connection available")
+        }
+
+    }
+
+
+    func connectedToNetwork() -> Bool{
+        var zeroAddress = sockaddr_in()
+        zeroAddress.sin_len = UInt8(MemoryLayout<sockaddr_in>.size)
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+
+        guard let defaultRouteReachability = withUnsafePointer(to: &zeroAddress, {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {
+                SCNetworkReachabilityCreateWithAddress(nil, $0)
+            }
+        }) else {
+            return false
+        }
+
+        var flags: SCNetworkReachabilityFlags = []
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability, &flags) {
+            return false
+        }
+
+        let isReachable = flags.contains(.reachable)
+        let needsConnection = flags.contains(.connectionRequired)
+
+        return (isReachable && !needsConnection)
     }
     
 
