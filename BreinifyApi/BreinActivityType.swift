4147a1fcb199613f6573dcc95f6c51c8d2bd2d48
Marco Recchioni <m.recchioni@me.com>
2020-11-30 22:36:41 +0100

    additional activity types added

--- BreinifyApi/BreinActivityType.swift
+++ BreinifyApi/BreinActivityType.swift
@@ -6,7 +6,6 @@
 import Foundation
 
 public enum BreinActivityType: String {
-
     case SEARCH = "search"
     case LOGIN = "login"
     case LOGOUT = "logout"
@@ -15,4 +14,18 @@ public enum BreinActivityType: String {
     case SELECT_PRODUCT = "selectProduct"
     case CHECKOUT = "checkOut"
     case OTHER = "other"
+    case PAGE_VISIT = "pageVisit"
+    case VIEWED_PRODUCT = "viewedProduct"
+    case CANCEL_ORDER = "cancelOrder"
+    case IDENTIFY = "identify"
+    case SEND_LOC = "sendLoc"
+    case OPEN_PUSH_NOTIFICATION = "openedPushNotification"
+    case FAIL_PUSH_NOTIFICATION = "failPushNotification"
+    case RECEIVED_PUSH_NOTIFICATION = "receivedPushNotification"
+    case START_PUSH_NOTIFICATION = "startPushNotification"
+    case STOP_PUSH_NOTIFICATION = "stopPushNotification"
+    case CHECKOUT_EMAIL_ENTERED = "checkOutEmailEntered"
+    case OPENED_EMAIL = "openedEmail"
+    case START_EMAIL = "startEmail"
+    case STOP_EMAIL = "stopEmail"
 }
