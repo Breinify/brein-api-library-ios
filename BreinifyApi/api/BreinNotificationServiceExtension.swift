65c2b6ee60b40ab25760361f599e51095212bd5a
Marco Recchioni <m.recchioni@me.com>
2021-01-06 14:50:25 +0100

    notification service extension added

---
+++ BreinifyApi/api/BreinNotificationServiceExtension.swift
@@ -0,0 +1,37 @@
+//
+// Created by Marco on 12/30/20.
+// Copyright (c) 2020 Breinify. All rights reserved.
+//
+
+import Foundation
+import UserNotifications
+
+@available(iOS 11.0, *)
+open class BreinNotificationServiceExtension: UNNotificationServiceExtension {
+
+    var contentHandler: ((UNNotificationContent) -> Void)?
+    var bestAttemptContent: UNMutableNotificationContent?
+
+    open override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+        self.contentHandler = contentHandler
+        bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
+
+        if let bestAttemptContent = bestAttemptContent {
+            // Modify the notification content here...
+            bestAttemptContent.title = "\(bestAttemptContent.title) [modified chaka]"
+            print("chakakaka")
+
+            contentHandler(bestAttemptContent)
+        }
+    }
+
+    open override func serviceExtensionTimeWillExpire() {
+        // Called just before the extension will be terminated by the system.
+        // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
+        if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
+            contentHandler(bestAttemptContent)
+        }
+    }
+
+
+}
